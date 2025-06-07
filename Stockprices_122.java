4f33b106b383f2e9dde975efd82f6ab1d7412dc7
rachanakeshaowar <keshrachana0@gmail.com>
2025-06-07 11:02:42 +0530

    122 leetcode stock prices

---
+++ Stockprices_122.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Stockprices_122 {
+
+    public int maxProfit(int[] prices) {
+        int ans =0;
+         for(int i=1;i<prices.length;i++){
+            if(prices[i]>prices[i-1]){
+                ans+=prices[i]-prices[i-1];
+            }
+         }
+         return ans ;
+    }
+}
+
