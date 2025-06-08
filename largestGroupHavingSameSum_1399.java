a6601e2ee6cb55c81984ca40e27197293becdc7c
rachanakeshaowar <keshrachana0@gmail.com>
2025-06-08 23:50:51 +0530

    1399 leetcode count largest group

---
+++ largestGroupHavingSameSum_1399.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class largestGroupHavingSameSumSolution_1399 {
+    public int countLargestGroup(int num) {
+        HashMap<Integer, Integer> grp = new HashMap<>();
+        int max = 0;
+
+        for (int val = 1; val <= num; val++) {
+            int sum = getSum(val);
+            grp.put(sum, grp.getOrDefault(sum, 0) + 1);
+            max = Math.max(max, grp.get(sum));
+        }
+
+        int cnt = 0;
+        for (int freq : grp.values()) {
+            if (freq == max) {
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+
+    private int getSum(int val) {
+        int sum = 0;
+        while (val > 0) {
+            sum += val % 10;
+            val /= 10;
+        }
+        return sum;
+    }
+}
+ 
