173d1dd3aac322a456a85061bd7561494a51e874
rachanakeshaowar <keshrachana0@gmail.com>
2025-06-16 18:11:27 +0530

    Three Consecutive Odds 1550

---
+++ ThreeConsecutiveOdds1550.java
@@ -0,0 +1,17 @@
+public class ThreeConsecutiveOdds1550 {
+
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int count = 0;
+        for (int num : arr) {
+            if (num % 2 != 0) {
+                count++;
+                if (count == 3) return true;
+            } else {
+                count = 0;
+            }
+        }
+        return false;
+    }
+}
+
+
