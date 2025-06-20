b881b9564648c8c8b2548062e2d152c24eb1f32a
rachanakeshaowar <keshrachana0@gmail.com>
2025-06-20 22:16:54 +0530

    Maximum SubArray 53

---
+++ MaximumSubarray53.java
@@ -0,0 +1,13 @@
+class MaximumSubarray53{
+    public int maxSubArray(int[] nums) {
+        int max = nums[0];
+        int sum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            sum = Math.max(nums[i], sum + nums[i]);
+            max = Math.max(max, sum);
+        }
+
+        return max;
+    }
+}
