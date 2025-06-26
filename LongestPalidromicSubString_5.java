d9b3d5c723ecca28618a9df381238fe221d24312
rachanakeshaowar <keshrachana0@gmail.com>
2025-06-26 22:54:49 +0530

    Longest Palindromic Substring 5

---
+++ LongestPalidromicSubString_5.java
@@ -0,0 +1,29 @@
+public class LongestPalidromicSubString_5 {
+ 
+    int maxLen = 0;
+    int lo = 0;
+    public String longestPalindrome(String s) {
+        char[] input = s.toCharArray();
+        if(s.length() < 2) {
+            return s;
+        }
+        
+        for(int i = 0; i<input.length; i++) {
+            expand(input, i, i);
+            expand(input, i, i+1);
+        }
+        return s.substring(lo, lo+maxLen);
+    }
+    
+    public void expand(char[] s, int j, int k) {
+        while(j >= 0 && k < s.length && s[j] == s[k]) {
+            j--;
+            k++;
+        }
+        if(maxLen < k - j - 1) {
+            maxLen = k - j - 1;
+            lo = j+1;
+        }
+    }
+}
+
